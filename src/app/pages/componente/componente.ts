c3a176fe737db63654cf85722f36577a56f268c8
renanbreier <renanoliveirabreier@gmail.com>
2025-11-12 20:29:31 -0300

    Refatoração: Página e Componente

--- src/app/pages/componente/componente.ts
+++ src/app/pages/componente/componente.ts
@@ -1,16 +1,40 @@
 import { Component } from '@angular/core';
-import {DxSelectBoxComponent} from 'devextreme-angular';
-import {IbgeCidade} from '../../shared/components/ibge-cidade/ibge-cidade';
+import { CommonModule } from '@angular/common';
+import { IbgeCidade } from '../../shared/components/ibge-cidade/ibge-cidade';
+import { DxPopupComponent, DxTemplateModule } from 'devextreme-angular';
+import {DxiPopupToolbarItemComponent} from 'devextreme-angular/ui/popup';
+import { Cidade } from '../../models/cidade';
 
 @Component({
   selector: 'app-componente',
   templateUrl: './componente.html',
   styleUrl: './componente.scss',
   imports: [
-    IbgeCidade
+    CommonModule,
+    IbgeCidade,
+    DxPopupComponent,
+    DxiPopupToolbarItemComponent,
+    DxTemplateModule
   ]
 })
 export class Componente {
+  popupVisivelPagina = false;
+  cidadeDetalhe: any = null;
+  closeButtonOptionsPagina: Record<string, unknown>;
 
-  protected readonly Component = Component;
+  constructor() {
+    this.closeButtonOptionsPagina = {
+      text: 'Close',
+      stylingMode: 'outlined',
+      type: 'normal',
+      onClick: () => {
+        this.popupVisivelPagina = false;
+      },
+    };
+  }
+
+  onCidadeSelecionada(cidade: Cidade) {
+    this.cidadeDetalhe = cidade;
+    this.popupVisivelPagina = true;
+  }
 }
--- src/app/shared/components/ibge-cidade/ibge-cidade.ts
+++ src/app/shared/components/ibge-cidade/ibge-cidade.ts
@@ -1,68 +1,62 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Estado} from "../../../models/estado";
 import {Cidade} from "../../../models/cidade";
-import {DxPopupComponent, DxSelectBoxComponent} from 'devextreme-angular';
+import {DxSelectBoxComponent} from 'devextreme-angular';
 import {ApiIbge} from '../../services/api-ibge';
-import {DxiPopupToolbarItemComponent} from 'devextreme-angular/ui/popup';
-import { DxTemplateModule } from 'devextreme-angular';
 
 @Component({
   selector: 'app-ibge-cidade',
   imports: [
     DxSelectBoxComponent,
-    DxPopupComponent,
-    DxiPopupToolbarItemComponent,
-    DxTemplateModule,
   ],
   templateUrl: './ibge-cidade.html',
   styleUrl: './ibge-cidade.scss',
   providers: [ApiIbge]
 })
-
-export class IbgeCidade {
+export class IbgeCidade implements OnInit {
   cidades: Cidade[] = [];
   estados: Estado[] = [];
   siglaEstado: string = '';
 
   cidadeSelecionada: any = null;
 
-  popupVisible = false;
+  // Input para receber o estado padrão
+  @Input() estadoPadrao: string = '';
 
-  closeButtonOptions: Record<string, unknown>;
+  // Output para emitir a cidade selecionada
+  @Output() cidadeSelecionadaEmitter = new EventEmitter<Cidade>();
 
   constructor(private service: ApiIbge) {
 
-    this.closeButtonOptions = {
-      text: 'Close',
-      stylingMode: 'outlined',
-      type: 'normal',
-      onClick: () => {
-        this.popupVisible = false;
-      },
-    };
   }
 
   ngOnInit(){
     this.service.getEstados().subscribe((e) => {
       this.estados = e;
-    })
+
+      // Verifica se um estado Padrão foi passado
+      if (this.estadoPadrao) {
+        this.selecionarEstado(this.estadoPadrao);
+      }
+    });
   }
 
-  onValueChanged(e: any) {
-    this.siglaEstado = e.value;
-
+  selecionarEstado(sigla: string) {
+    this.siglaEstado = sigla;
     this.service.getCidades(this.siglaEstado).subscribe((c) =>{
       this.cidades = c;
     });
   }
 
-  showInfo(e: any) {
-    this.cidadeSelecionada = e.selectedItem;
-    console.log('Cidade selecionada:', this.cidadeSelecionada);
-
-    if(this.cidadeSelecionada) {
-      this.popupVisible = true;
-    }
+  onValueChanged(e: any) {
+    this.selecionarEstado(e.value);
   }
 
+  showInfo(e: any) {
+    this.cidadeSelecionada = e.selectedItem;
+
+    if(this.cidadeSelecionada) {
+      this.cidadeSelecionadaEmitter.emit(this.cidadeSelecionada);
+    }
+  }
 }
